da71109b4db3a221261509a948b7d5d130b07039
PavaniV57 <5798pavani@gmail.com>
2025-07-24 07:00:36 -0700

    Update application.py

--- application.py
+++ application.py
@@ -112,4 +112,4 @@ def list_csv_reports():
 @app.route('/api/slow-endpoint')
 def slow_endpoint():
     time.sleep(5)
-    return "This was a slow response after 5 seconds
+    return "This was a slow response after 5 seconds"
